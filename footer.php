2bd3e4250b0e7e8dee4fee12f2cdb5828d9dff01
isabelc <me@isabelcastillo.com>
2014-06-03 19:33:09 -0400

    initial

---
+++ footer.php
@@ -0,0 +1,5 @@
+<?php
+/**
+ * The template for displaying the footer
+ */
+?></div><footer id="colophon" class="site-footer" role="contentinfo"><?php get_sidebar( 'footer' ); ?><div class="site-info"><?php do_action( 'twentyfourteen_credits' ); ?></div></footer></div><?php wp_footer(); ?></body></html>
