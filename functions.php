8ef65168d8e01ad1adf87d2de891d5ad8ae9209a
isabelc <me@isabelcastillo.com>
2014-06-04 20:27:35 -0400

    minified

--- functions.php
+++ functions.php
@@ -1,28 +1,6 @@
 <?php
 /**
  * Twenty Fourteen functions and definitions
- *
- * Set up the theme and provides some helper functions, which are used in the
- * theme as custom template tags. Others are attached to action and filter
- * hooks in WordPress to change core functionality.
- *
- * When using a child theme you can override certain functions (those wrapped
- * in a function_exists() call) by defining them first in your child theme's
- * functions.php file. The child theme's functions.php file is included before
- * the parent theme's file, so the child theme functions would be used.
- *
- * @link http://codex.wordpress.org/Theme_Development
- * @link http://codex.wordpress.org/Child_Themes
- *
- * Functions that are not pluggable (not wrapped in function_exists()) are
- * instead attached to a filter or action hook.
- *
- * For more information on hooks, actions, and filters,
- * @link http://codex.wordpress.org/Plugin_API
- *
- * @package WordPress
- * @subpackage Twenty_Fourteen
- * @since Twenty Fourteen 1.0
  */
 
 /**
@@ -30,7 +8,6 @@
  *
  * @see twentyfourteen_content_width()
  *
- * @since Twenty Fourteen 1.0
  */
 if ( ! isset( $content_width ) ) {
 	$content_width = 474;
@@ -57,14 +34,6 @@ if ( ! function_exists( 'twentyfourteen_setup' ) ) :
  */
 function twentyfourteen_setup() {
 
-	/*
-	 * Make Twenty Fourteen available for translation.
-	 *
-	 * Translations can be added to the /languages/ directory.
-	 * If you're building a theme based on Twenty Fourteen, use a find and
-	 * replace to change 'twentyfourteen' to the name of your theme in all
-	 * template files.
-	 */
 	load_theme_textdomain( 'twentyfourteen', get_template_directory() . '/languages' );
 
 	// This theme styles the visual editor to resemble the theme style.
