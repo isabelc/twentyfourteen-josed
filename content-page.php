32dfb86faed2b3189067115fda3e4dbf32d74d92
Isabel Castillo <me@isabelcastillo.com>
2015-05-04 16:51:24 -0400

    initial

---
+++ content-page.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * The template used for displaying page content
+ */
+?><article id="post-<?php the_ID(); ?>" <?php post_class(); ?>><?php
+		// Page thumbnail and title.
+		twentyfourteen_post_thumbnail();
+		the_title( '<header class="entry-header"><h1 class="entry-title">', '</h1></header>' );
+?><div class="entry-content"><?php
+the_content();
+wp_link_pages( array(
+				'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'twentyfourteen' ) . '</span>',
+				'after'       => '</div>',
+				'link_before' => '<span>',
+				'link_after'  => '</span>',
+			) );
+edit_post_link( __( 'Edit', 'twentyfourteen' ), '<span class="edit-link">', '</span>' );
+?></div></article>
